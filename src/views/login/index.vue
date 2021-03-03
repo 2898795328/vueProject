aede06844aad06d2c5207912269628d787d4ad45
2898795328 <2898795328@qq.com>
2021-03-03 22:18:22 +0800

    新项目

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,13 @@
 <template>
 <div id="login">
-    <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">
-  点我打开
-</el-button>
-<el-drawer
-  title="我是标题" :visible.sync="drawer" :with-header="false">
-  <span>我来啦!</span>
-</el-drawer>
-
+  <el-row>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
 </div>
 </template>
 
@@ -17,7 +17,7 @@ export default {
   props:{},
   data(){
     return {
-      drawer: false,
+     
     };
   },
   watch:{},
