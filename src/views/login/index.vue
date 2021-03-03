2b16feded72b6e29783c03bd1dbfaa89075c5877
2898795328 <2898795328@qq.com>
2021-03-03 21:49:05 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ src/views/login/index.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="login">
+    <p class="aa">登录页面gkjsagdsdjgk</p>
+</div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style lang="scss" scoped>
+#login{
+  height: 50vh;
+  background-color: rgb(53, 21, 21);
+}
+.aa{
+  font-size: 20px;
+}
+</style>
